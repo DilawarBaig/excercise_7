55eb8f8bb47df76351db3a7fa759451772b191fb
DilawarBaig <dilawaralibaig@gmail.com>
2023-09-23 16:04:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Addition
+let addition = 5 + 3;
+console.log(`addition result: ${addition}`);
+// subtraction
+let subraction = 16 - 8;
+console.log(`subtraction result: ${subraction}`);
+// multiplication
+let multiplication = 4 * 2;
+console.log(`multiplication result: ${multiplication}`);
+// division
+let division = 72 / 9;
+console.log(`division result: ${division}`);
---
+++ main.ts
@@ -0,0 +1,17 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// Addition
+let addition:number = 5 + 3;
+console.log(`addition result: ${addition}`);
+
+// subtraction
+let subraction:number = 16 - 8;
+console.log(`subtraction result: ${subraction}`);
+
+// multiplication
+let multiplication:number = 4 * 2;
+console.log(`multiplication result: ${multiplication}`);
+
+// division
+let division:number = 72 / 9;
+console.log(`division result: ${division}`);
